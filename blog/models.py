0aaae541bdd9e28b28cb7376f2d9a63812f91ce9
AiGaA <>
2023-08-29 01:27:06 +0000

    Create a Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
 # Create your models here.
+
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_post")
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    image = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    excerpt = models.TextField(blank=True)
+
+    class Meta:
+        orderin = ['-created_on']
+
+    def __str__(self):
+        return self.title
+    
+    def number_of_likes(self):
+        return self.likes.count()
+    
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 
