c857b9619fceb755b0a0f560681d381b602e851b
AiGaA <>
2023-08-31 00:37:59 +0000

    Fix the register issue, user can create an account

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,8 @@
 from django.shortcuts import render
 from django.views import View
-from django.contrib.auth.forms import UserCreationForm
-from .forms import CreateUserForm
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'blog/index.html')
-
-
-def registerPage(request):
-    form = CreateUserForm()
-
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-
-    context = {'form': form}
-    return render(request, 'blog/register.html', context)
-
-
-def loginPage(request):
-    context = {}
-    return render(request, 'blog/login.html', context)
